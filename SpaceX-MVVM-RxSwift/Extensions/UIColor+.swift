50a2f8edac834b3c38128ed026438569ead537c1
KorobskoyRoman <shopone023@gmail.com>
2022-10-01 21:42:54 +0300

    Added ext + constants

---
+++ SpaceX-MVVM-RxSwift/Extensions/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 01.10.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static func mainBackground() -> UIColor {
+        return #colorLiteral(red: 0.937254902, green: 0.9529411765, blue: 0.9647058824, alpha: 1)
+    }
+}
