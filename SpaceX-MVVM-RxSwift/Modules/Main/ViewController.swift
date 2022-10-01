5a4bf847304618b719dc73a58e6dce84297fa046
KorobskoyRoman <shopone023@gmail.com>
2022-10-01 21:15:33 +0300

    Initial Commit

---
+++ SpaceX-MVVM-RxSwift/Modules/Main/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 01.10.2022.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
