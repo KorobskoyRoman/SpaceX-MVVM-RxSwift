8c482b021cb5e2fcb761868b39044439d6d96697
KorobskoyRoman <shopone023@gmail.com>
2022-10-03 19:01:31 +0300

    Implemented viewModel + UI

---
+++ SpaceX-MVVM-RxSwift/Modules/Main/MainViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewModel.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 02.10.2022.
+//
+
+import RxSwift
+import RxCocoa
+
+protocol MainViewModelType {
+    var launches: BehaviorRelay<[LaunchInfo]> { get set }
+    var reload: (()->Void)? { get set }
+    func getLaunches()
+    func push()
+}
+
+final class MainViewModel: MainViewModelType {
+    var launches: BehaviorRelay<[LaunchInfo]> = BehaviorRelay<[LaunchInfo]>(value: [])
+    var reload: (() -> Void)?
+    private lazy var fetchedLaunches = [LaunchInfo]()
+    private let networkingService: NetworkService
+
+    init(networkingService: NetworkService) {
+        self.networkingService = networkingService
+        self.getLaunches()
+        reload?()
+    }
+
+    func getLaunches()  {
+        networkingService.fetchLaunches { results in
+            self.launches.accept(results)
+            self.reload?()
+        }
+    }
+
+    func push() {
+        print(#function)
+    }
+}
--- SpaceX-MVVM-RxSwift/Modules/Main/View/MainViewController.swift
+++ SpaceX-MVVM-RxSwift/Modules/Main/View/MainViewController.swift
@@ -6,23 +6,173 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class MainViewController: UIViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, LaunchInfo>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, LaunchInfo>
+
+    private lazy var collectionView = UICollectionView(frame: view.bounds,
+                                                       collectionViewLayout: createCompositialLayout())
+    private lazy var dataSource = createDiffableDataSource()
+    private let disposeBag = DisposeBag()
+
+    private var viewModel: MainViewModelType
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.getLaunches()
         setupView()
-        NetworkService().fetchLaunches { result in
-            print(result)
-        }
-        NetworkService().fetchRocket(id: "5e9d0d95eda69955f709d1eb") { result in
-            print(result)
-        }
+        bind()
+        reload()
+    }
+
+    init(viewModel: MainViewModelType) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        reload()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     private func setupView() {
         view.backgroundColor = .mainBackground()
         navigationController?.navigationBar.prefersLargeTitles = true
         title = MainConstants.mainTitle
+        setupCollectionView()
+    }
+
+    private func reload() {
+        viewModel.reload = {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.applySnapshot()
+            }
+        }
+    }
+
+    private func setupCollectionView() {
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(collectionView)
+
+        collectionView.register(MainCell.self, forCellWithReuseIdentifier: MainCell.reuseId)
+        collectionView.register(SectionHeader.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: SectionHeader.reuseId)
     }
 }
 
+extension MainViewController {
+    private func bind() {
+
+    }
+}
+
+// MARK: - Create layout
+extension MainViewController {
+    private func createCompositialLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+            guard let section = Section(rawValue: sectionIndex) else {
+                fatalError("Section not found")
+            }
+            switch section {
+            case .mainSection:
+                return self.createMainSection()
+            }
+        }
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = HelpConstants.Constraints.interSectionSpacing
+        layout.configuration = config
+        return layout
+    }
+
+    private func createMainSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(HelpConstants.Constraints.cellWidth),
+                                              heightDimension: .absolute(HelpConstants.Constraints.cellHeight))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: HelpConstants.Constraints.topItemInset,
+            leading: HelpConstants.Constraints.itemInset,
+            bottom: HelpConstants.Constraints.itemInset,
+            trailing: HelpConstants.Constraints.itemInset)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalWidth(0.4))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitem: item,
+                                                       count: HelpConstants.Constraints.groupSize)
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        section.interGroupSpacing = HelpConstants.Constraints.interGroupSpacing
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: HelpConstants.Constraints.topItemInset,
+            leading: HelpConstants.Constraints.itemInset,
+            bottom: .zero,
+            trailing: HelpConstants.Constraints.itemInset)
+
+        return section
+    }
+
+    private func createHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.5),
+                                                       heightDimension: .fractionalHeight(0.05))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
+                                                                        elementKind:
+                                                                            UICollectionView.elementKindSectionHeader,
+                                                                        alignment: .top)
+
+        return sectionHeader
+    }
+}
+
+// MARK: - Create data source
+extension MainViewController {
+    private func createDiffableDataSource() -> DataSource {
+        let dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, data in
+            guard let section = Section(rawValue: indexPath.section) else {
+                fatalError("No section")
+            }
+            switch section {
+            case .mainSection:
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainCell.reuseId,
+                                                              for: indexPath) as? MainCell
+                else { return UICollectionViewCell() }
+                cell.configure(with: data)
+                return cell
+            }
+        }
+
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard let sectionHeader = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: SectionHeader.reuseId,
+                for: indexPath)
+                    as? SectionHeader else { fatalError("can't create new section header")}
+            guard let section = Section(rawValue: indexPath.section) else { fatalError("No section kind") }
+            sectionHeader.configurate(text: section.description(), font: UIFont.systemFont(ofSize: 15), textColor: .gray)
+
+            return sectionHeader
+        }
+
+        return dataSource
+    }
+
+    private func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+
+        snapshot.appendSections([.mainSection])
+        snapshot.appendItems(viewModel.launches.value, toSection: .mainSection)
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+    }
+}
