d67be6938240e23034bca4afc8c79caecca97dc0
KorobskoyRoman <shopone023@gmail.com>
2022-10-04 22:07:08 +0300

    Added view activity extension

---
+++ SpaceX-MVVM-RxSwift/Extensions/UIView+.swift
@@ -0,0 +1,60 @@
+//
+//  UIView+.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 04.10.2022.
+//
+
+import UIKit
+
+extension UIView {
+    static let loadingViewTag = 1938123987
+    static let labelInfoTag = 1938123988
+
+    func showLoading(style: UIActivityIndicatorView.Style = .large,
+                     color: UIColor? = nil,
+                     text: String = ViewConstants.loadingText) {
+        DispatchQueue.main.async {
+            var loading = self.viewWithTag(UIImageView.loadingViewTag) as? UIActivityIndicatorView
+            if loading == nil {
+                loading = UIActivityIndicatorView(style: style)
+            }
+            // label setup
+            let infoLabel: UILabel = {
+                let label = UILabel()
+                label.text = text
+                label.font = .systemFont(ofSize: 13, weight: .light)
+                label.translatesAutoresizingMaskIntoConstraints = false
+                return label
+            }()
+
+            guard let loading = loading else { return }
+            loading.translatesAutoresizingMaskIntoConstraints = false
+            loading.startAnimating()
+            loading.hidesWhenStopped = true
+            loading.tag = UIView.loadingViewTag
+            infoLabel.tag = UIView.labelInfoTag
+            self.addSubview(loading)
+            self.addSubview(infoLabel)
+            loading.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            loading.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+            infoLabel.centerYAnchor.constraint(equalTo: loading.centerYAnchor, constant: 25).isActive = true
+            infoLabel.centerXAnchor.constraint(equalTo: loading.centerXAnchor).isActive = true
+
+            if let color = color {
+                loading.color = color
+                infoLabel.textColor = color
+            }
+        }
+    }
+
+    func stopLoading() {
+        DispatchQueue.main.async {
+            let loading = self.viewWithTag(UIView.loadingViewTag) as? UIActivityIndicatorView
+            let infoLabel = self.viewWithTag(UIView.labelInfoTag) as? UILabel
+            loading?.stopAnimating()
+            loading?.removeFromSuperview()
+            infoLabel?.removeFromSuperview()
+        }
+    }
+}
