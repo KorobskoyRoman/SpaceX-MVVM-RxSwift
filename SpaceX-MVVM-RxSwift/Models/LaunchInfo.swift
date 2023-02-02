eedb046cc8319741902e5df1d41031d5784b48b9
KorobskoyRoman <shopone023@gmail.com>
2023-02-03 02:00:57 +0300

    feat: Update rocket model

--- SpaceX-MVVM-RxSwift/Models/LaunchInfo.swift
+++ SpaceX-MVVM-RxSwift/Models/LaunchInfo.swift
@@ -42,18 +42,51 @@ struct Patch: Decodable {
 
 // MARK: - Rockets
 struct Rocket: Decodable {
+    let height, diameter: Size
+    let mass: Mass
+    let payloadWeights: [PayloadWeight]
     let flickrImages: [String]?
     let name, type: String?
     let id: String?
 
     enum CodingKeys: String, CodingKey {
         case flickrImages = "flickr_images"
-        case name, type, id
+        case payloadWeights = "payload_weights"
+        case name, type, id, height, diameter, mass
     }
+}
+
+struct Size: Decodable {
+    let meters, feet: Double?
+}
+
+struct Mass: Decodable {
+    let kg, lb: Double
+}
+
+struct PayloadWeight: Decodable {
+    let id, name: String
+    let kg, lb: Double
 }
 
 extension Rocket {
     static var emptyRocket: Rocket {
-        return Rocket(flickrImages: [], name: "", type: "", id: "")
+        Rocket(
+            height: Size(meters: 0, feet: 0),
+            diameter: Size(meters: 0, feet: 0),
+            mass: Mass(kg: 0, lb: 0),
+            payloadWeights: [
+                PayloadWeight(
+                    id: "",
+                    name: "",
+                    kg: 0,
+                    lb: 0
+                )
+            ],
+            flickrImages: [],
+            name: "",
+            type: "",
+            id: ""
+        )
     }
 }
