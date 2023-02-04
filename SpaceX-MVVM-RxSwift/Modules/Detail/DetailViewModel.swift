be2dc11086b04fd4322628664b6ebfbf604dd28a
KorobskoyRoman <shopone023@gmail.com>
2023-02-04 04:51:49 +0300

    fix: Delete useless func

--- SpaceX-MVVM-RxSwift/Modules/Detail/DetailViewModel.swift
+++ SpaceX-MVVM-RxSwift/Modules/Detail/DetailViewModel.swift
@@ -9,13 +9,9 @@ import RxSwift
 import RxCocoa
 
 protocol DetailViewModelType {
-    var launchInfo: BehaviorRelay<LaunchInfo>? { get set }
     var title: String { get }
     var image: String { get }
-    var name: Observable<String?> { get }
-    var date: Observable<String> { get }
-//    var country: Observable<String> { get }
-//    var cost: Observable<String> { get }
+    var launchInfo: BehaviorRelay<LaunchInfo>? { get set }
     var rocketInfo: BehaviorRelay<Rocket>? { get set }
     func getRocketInfo()
 }
@@ -24,6 +20,14 @@ final class DetailViewModel: DetailViewModelType {
     var launchInfo: BehaviorRelay<LaunchInfo>?
     var rocketInfo: BehaviorRelay<Rocket>? = BehaviorRelay<Rocket>(value: .emptyRocket)
     private let networkSerivce: NetworkService
+
+    var title: String {
+        launchInfo?.value.name ?? "n/a"
+    }
+
+    var image: String {
+        rocketInfo?.value.flickrImages?.randomElement() ?? ""
+    }
 
     init(networkSerivce: NetworkService, launchInfo: BehaviorRelay<LaunchInfo>?) {
         self.networkSerivce = networkSerivce
@@ -37,44 +41,4 @@ final class DetailViewModel: DetailViewModelType {
             self.rocketInfo?.accept(rocket)
         }
     }
-
-    private func getDate(_ date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "d MMM, yyyy"
-        return formatter.string(from: date)
-    }
-
-    private func getCost(_ cost: Int) -> String {
-        let formatter = NumberFormatter()
-        formatter.usesGroupingSeparator = true
-        formatter.numberStyle = .currency
-        formatter.locale = Locale.current
-        return formatter.string(from: NSNumber(value: cost)) ?? "n/a"
-    }
-}
-
-extension DetailViewModel {
-    var title: String {
-        return launchInfo?.value.name ?? ""
-    }
-
-    var image: String {
-        return rocketInfo?.value.flickrImages?.first ?? ""
-    }
-
-    var name: Observable<String?> {
-        return Observable.just(launchInfo?.value.name)
-    }
-
-    var date: Observable<String> {
-        return Observable.just(getDate(launchInfo?.value.dateUTC ?? .now))
-    }
-
-//    var country: Observable<String> {
-//        return Observable.just(rocketInfo?.value.country ?? "n/a")
-//    }
-//
-//    var cost: Observable<String> {
-//        return Observable.just(getCost(rocketInfo?.value.costPerLaunch ?? 0))
-//    }
 }
