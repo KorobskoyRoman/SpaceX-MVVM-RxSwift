1d9f3aee01ac86646f81f80be0720dad53f9af94
KorobskoyRoman <shopone023@gmail.com>
2023-02-14 21:11:36 +0300

    feat: User defaults service

---
+++ SpaceX-MVVM-RxSwift/Services/UserDafaults/UserDafaults+Keys.swift
@@ -0,0 +1,17 @@
+//
+//  UserDafaults+Keys.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 13.02.2023.
+//
+
+enum UserDefaultsKeys: String {
+//    static let height = "height"
+//    static let diameter = "diameter"
+//    static let mass = "mass"
+//    static let weight = "weight"
+    case height
+    case diameter
+    case mass
+    case weight
+}
