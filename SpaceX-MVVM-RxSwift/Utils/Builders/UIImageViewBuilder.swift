d2248437e68a2a6b6084c64faa80b9add6969dbf
KorobskoyRoman <shopone023@gmail.com>
2023-02-16 01:52:22 +0300

    util: UIImage builder

---
+++ SpaceX-MVVM-RxSwift/Utils/Builders/UIImageViewBuilder.swift
@@ -0,0 +1,61 @@
+//
+//  UIImageViewBuilder.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 16.02.2023.
+//
+
+import UIKit
+
+final class UIImageViewBuilder {
+    public private(set) var translatesAutoresizing: Bool = false
+    public private(set) var backgroundColor: UIColor = .clear
+    public private(set) var clipToBounds: Bool = true
+    public private(set) var contentMode: UIView.ContentMode = .scaleAspectFill
+    public private(set) var cornerRadius: CGFloat = 0
+}
+
+extension UIImageViewBuilder {
+    @discardableResult
+    public func contentMode(_ mode: UIView.ContentMode) -> UIImageViewBuilder {
+        contentMode = mode
+        return self
+    }
+
+    @discardableResult
+    public func cornerRadius(_ radius: CGFloat) -> UIImageViewBuilder {
+        cornerRadius = radius
+        return self
+    }
+
+    @discardableResult
+    public func translatesAutoresizingMaskIntoConstraints(_ flag: Bool) -> UIImageViewBuilder {
+        translatesAutoresizing = flag
+        return self
+    }
+
+    @discardableResult
+    public func backgroundColor(_ color: UIColor) -> UIImageViewBuilder {
+        backgroundColor = color
+        return self
+    }
+
+    public func build() -> UIImageView {
+        return build(imageView: UIImageView())
+    }
+
+    public func buildWithShimmer() -> ShimmerUIImageView {
+        return build(imageView: ShimmerUIImageView())
+    }
+}
+
+private extension UIImageViewBuilder {
+    func build<T: UIImageView>(imageView: T) -> T {
+        imageView.translatesAutoresizingMaskIntoConstraints = translatesAutoresizing
+        imageView.backgroundColor = backgroundColor
+        imageView.clipsToBounds = clipToBounds
+        imageView.contentMode = contentMode
+        imageView.layer.cornerRadius = cornerRadius
+        return imageView
+    }
+}
