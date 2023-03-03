1b15b786f313fdfbd6acf9d1e7083ab8418cf331
KorobskoyRoman <shopone023@gmail.com>
2023-03-03 20:39:51 +0300

    feat: Implement alert view

--- SpaceX-MVVM-RxSwift/Modules/Splash/SplashViewController.swift
+++ SpaceX-MVVM-RxSwift/Modules/Splash/SplashViewController.swift
@@ -8,17 +8,22 @@
 import UIKit
 
 final class SplashViewController: UIViewController {
-    private let viewModel: SplashViewModelType
+    private var viewModel: SplashViewModelType
     private let image: UIImageView = {
         return UIImageViewBuilder()
             .contentMode(.scaleAspectFit)
             .build()
     }()
+    private lazy var animationContainer = InfoAlert()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         push()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        viewModel.stopNotify()
     }
 
     init(viewModel: SplashViewModelType) {
@@ -34,11 +39,55 @@ final class SplashViewController: UIViewController {
 private extension SplashViewController {
     func setupView() {
         navigationController?.navigationBar.isHidden = true
-        view.showLoading(yConstant: 100)
+        view.showLoading(yConstant: 200)
         image.image = R.image.spaceXLogo()
+
+        animationContainer = InfoAlert(
+            frame: CGRect(
+                x: self.view.frame.minX,
+                y: self.view.frame.minY,
+                width: 300,
+                height: 230
+            )
+        )
+
+        animationContainer.center = view.center
+        animationContainer.isHidden = true
+
         view.addSubview(image)
+        view.addSubview(animationContainer)
         view.backgroundColor = .mainBackground()
         setConstraints()
+
+        viewModel.showError = { [weak self] error in
+            guard let self else { return }
+            DispatchQueue.main.async {
+                self.animationContainer.infoLabel.text = error
+            }
+            self.animationContainer.actionCancel = {
+                UIView.animate(withDuration: 0.3) {
+                    self.animationContainer.alpha = 0.0
+                    self.animationContainer.isHidden = true
+                }
+                self.viewModel.hasError = false
+                self.push()
+            }
+            self.animationContainer.actionRetry = {
+                UIView.animate(withDuration: 0.3) {
+                    self.animationContainer.alpha = 0.0
+                    self.animationContainer.isHidden = true
+                }
+                Task {
+                    try? await Task.sleep(nanoseconds: 2_000_000)
+                    self.viewModel.fetchLaunches()
+                }
+            }
+
+            UIView.animate(withDuration: 1) {
+                self.animationContainer.isHidden = false
+                self.animationContainer.alpha = 1.0
+            }
+        }
     }
 
     func setConstraints() {
@@ -53,9 +102,11 @@ private extension SplashViewController {
 
 private extension SplashViewController {
     func push() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.view.stopLoading()
-            self.viewModel.push()
+        if !viewModel.hasError {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.view.stopLoading()
+                self.viewModel.push()
+            }
         }
     }
 }
