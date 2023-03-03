75aff42e36b171907b59dddb189db5c6ed903de4
KorobskoyRoman <shopone023@gmail.com>
2023-03-03 20:40:46 +0300

    feat: Update viewModel with alert methods

--- SpaceX-MVVM-RxSwift/Modules/Splash/SplashViewModel.swift
+++ SpaceX-MVVM-RxSwift/Modules/Splash/SplashViewModel.swift
@@ -7,24 +7,49 @@
 
 import RxSwift
 import RxCocoa
+import RxReachability
+import Reachability
 
 protocol SplashViewModelType {
+    var showError: ((String) -> Void)? { get set }
+    var hasError: Bool { get set }
+    func fetchLaunches()
     func push()
+    func stopNotify()
 }
 
 final class SplashViewModel: SplashViewModelType {
     weak var coordinator: AppCoodrinator?
+
+    var showError: ((String) -> Void)?
+    var hasError: Bool = false
 
     private var launches = BehaviorRelay<[LaunchInfo]>(value: [])
 
     private let networkingService: NetworkServiceType
     private let storageManager: LaunchesStorageType
     private let bag = DisposeBag()
+    private var reachability = try? Reachability()
 
     init(networkingService: NetworkServiceType,
          storage: LaunchesStorageType) {
         self.networkingService = networkingService
         self.storageManager = storage
+        getLaunches()
+        startNotify()
+        bind()
+    }
+
+    private func startNotify() {
+        try? reachability?.startNotifier()
+    }
+
+    func stopNotify() {
+        reachability?.stopNotifier()
+    }
+
+    func fetchLaunches() {
+        hasError = false
         getLaunches()
     }
 
@@ -42,9 +67,23 @@ final class SplashViewModel: SplashViewModelType {
 
                 try storageManager.save(launches: launches)
             } catch {
+                self.hasError = true
+                showError?(error.localizedDescription)
                 print(error.localizedDescription)
             }
         }
+    }
+
+    private func bind() {
+        reachability?.rx.isReachable
+            .subscribe (onNext: { isReachable in
+                if !isReachable {
+                    self.showError?(
+                        ReachabilityError.unavailable.description + "\nВы можете нажать \"Повторить\" для повторной загрузки. \nПри нажатии \"Продолжить\" будут загружены ранее полученные данные."
+                    )
+                }
+            })
+            .disposed(by: bag)
     }
 
     func push() {
