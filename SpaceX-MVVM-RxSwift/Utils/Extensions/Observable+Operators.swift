d07a7f7d81ffd93d6281a7ca877bfa4ad305bdb0
KorobskoyRoman <shopone023@gmail.com>
2023-07-14 21:15:41 +0300

    feat: Observable ext

---
+++ SpaceX-MVVM-RxSwift/Utils/Extensions/Observable+Operators.swift
@@ -0,0 +1,43 @@
+//
+//  Observable+Operators.swift
+//  SpaceX-MVVM-RxSwift
+//
+//  Created by Roman Korobskoy on 12.07.2023.
+//
+
+import RxSwift
+import RxCocoa
+
+protocol OptionalType {
+    associatedtype Wrapped
+
+    var value: Wrapped? { get }
+}
+
+extension Optional: OptionalType {
+    var value: Wrapped? {
+        return self
+    }
+}
+
+extension ObservableType where Element: OptionalType {
+    func filterNil() -> Observable<Element.Wrapped> {
+        return flatMap { (element) -> Observable<Element.Wrapped> in
+            if let value = element.value {
+                return .just(value)
+            } else {
+                return .empty()
+            }
+        }
+    }
+
+    func replaceNilWith(_ nilValue: Element.Wrapped) -> Observable<Element.Wrapped> {
+        return flatMap { (element) -> Observable<Element.Wrapped> in
+            if let value = element.value {
+                return .just(value)
+            } else {
+                return .just(nilValue)
+            }
+        }
+    }
+}
